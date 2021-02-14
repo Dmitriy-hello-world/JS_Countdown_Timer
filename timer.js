b5a3df94ef2b23fd4b643cf16b1cba07faea36ca
Dmitriy <easportstng@gmail.com>
2021-02-14 17:03:44 +0200

    first

---
+++ timer.js
@@ -0,0 +1,59 @@
+function getZero(num) {
+    if (num >= 0 && num < 10) {
+        return "0" + num;
+    } else {
+        return num;
+    }
+}
+
+// classSelector is main selector of clock
+
+function timer(classSelector,deadlineTime) {
+    
+    function getTime(deadlineTime) {
+        const t = Date.parse(deadlineTime) - Date.parse(new Date()),
+              days = Math.floor( t / (1000 * 60 * 60 * 24)),
+              hours = Math.floor( (t / (1000 * 60 * 60)) % 24),
+              minutes = Math.floor( (t / (1000 * 60)) % 60),
+              seconds = Math.floor( (t / 1000) % 60);
+
+        return {
+            t: t,
+            days: days,
+            hours: hours,
+            minutes: minutes,
+            seconds: seconds
+        };
+    }
+
+    // days|hours|minutes|seconds - are selectors for your clock
+
+    function mainTimer(selector, deadlineTime) {
+        const timer = document.querySelector(selector),
+              days = timer.querySelector("#days"),
+              hours = timer.querySelector("#hours"),
+              minutes = timer.querySelector("#minutes"),
+              seconds = timer.querySelector("#seconds"),
+              clockInterval = setInterval(updateClock,1000);
+
+        updateClock();      
+
+        function updateClock() {
+            const obj = getTime(deadlineTime);
+
+            days.innerHTML = getZero(obj.days);
+            hours.innerHTML = getZero(obj.hours);
+            minutes.innerHTML = getZero(obj.minutes);
+            seconds.innerHTML = getZero(obj.seconds);
+
+            if ( obj.t <= 0 ) {
+                clearInterval(clockInterval);
+            }
+        }
+    }
+
+    mainTimer(classSelector, deadlineTime);
+}
+
+// export default timer;
+// export {getZero};
